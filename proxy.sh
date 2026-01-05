703f3da7ba479218b60298dc973b8b88aff065c4
t0twn <t.t.w.n@outlook.com>
2026-01-06 00:39:40 +0800

    create env.sh manually, install docker compose automatically and bug fix

--- proxy.sh
+++ proxy.sh
@@ -6,23 +6,31 @@ KONG_INITED="$CURRENT_DIR/kong_inited"
 POSTGRES_PASSWORD="$CURRENT_DIR/POSTGRES_PASSWORD"
 SUB_APP_DIR="app"
 
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+CLI_PLUGINS_DIR="$DOCKER_CONFIG/cli-plugins"
+DOCKER_COMPOSE="$CLI_PLUGINS_DIR/docker-compose"
+COMPOSE_URL="https://github.com/docker/compose/releases/latest/download/docker-compose-linux-`uname -m`"
+
 
 up(){
     app=$1
-    test -f env.sh || { test -f env.sh.sample && cp env.sh.sample env.sh; }
-    test -f env.sh && source env.sh || echo NO global env.sh set
     cd $app && { test -f env.sh && source env.sh; }; docker compose -p kong up -d
 }
 
 down(){
     app=$1
-    cd $app && docker compose -p kong down -d
+    cd $app && { test -f docker-compose.yaml && docker compose -p kong -f docker-compose.yaml down || docker compose -p kong -f docker-compose.yml down;}
 }
 
 init(){
+
     test -f $KONG_INITED && return
 
     test -f $POSTGRES_PASSWORD || uuidgen >$POSTGRES_PASSWORD
+
+    test -f env.sh && source env.sh || { echo env.sh not found. Please copy env.sh.sample to env.sh and customize it as needed.; exit 1;}
+
+    test -f $DOCKER_COMPOSE || { mkdir -p $CLI_PLUGINS_DIR && curl -SL $COMPOSE_URL -o $DOCKER_COMPOSE && chmod +x $DOCKER_COMPOSE;}
 
     mkdir -p /etc/haproxy/map/ && \
     touch /etc/haproxy/map/{301,backend} && \
